8436eec64274850a5f34cc4872e352f5602474a4
Romanos Skiadas <rski@arista.com>
2018-02-14 19:11:15 +0000

    require use-package to fix byte compilation

--- init.el
+++ init.el
@@ -50,6 +50,7 @@
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+(require 'use-package)
 
 (setq use-package-always-ensure t)
 (package-refresh-contents t)
