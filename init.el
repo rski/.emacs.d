aed1f9842c15cff9d2e1840b5a1bc50b15f55e36
Romanos Skiadas <rski@arista.com>
2020-05-12 12:31:30 +0100

    remove centaur tabs
    notafan

--- init.el
+++ init.el
@@ -686,19 +686,3 @@ I always end up doing it in current buffer so might as well wrap it."
   ;; e.g. starting a non-daemon without having started a login shell, but
   ;; that will probably be too infrequent to matter.
   :config (keychain-refresh-environment))
-
-(use-package centaur-tabs
-  :demand
-  :config
-  (centaur-tabs-mode t)
-  :custom
-  (centaur-tabs-gray-out-icons 'buffer)
-  (centaur-tabs-style "rounded")
-  (centaur-tabs-height 36)
-  (centaur-tabs-set-icons t)
-  (centaur-tabs-set-modified-marker t)
-  (centaur-tabs-modified-marker "●")
-  (centaur-tabs-buffer-groups-function #'centaur-tabs-projectile-buffer-groups)
-
-  :bind (("s-[" . #'centaur-tabs-backward)
-	   ("s-]" . #'centaur-tabs-forward)))
